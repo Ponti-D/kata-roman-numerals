b0c0d5480d8823835ad67fbb60b3c4fb6dee8760
Ponti-D <spaediaz@gmail.com>
2022-12-22 19:44:10 +0000

    added more roman values and testcases

--- scr/numbersToRomanNumerals.js
+++ scr/numbersToRomanNumerals.js
@@ -1,5 +1,15 @@
-function convertNumbersToRomanNumerals(number){
+function convertNumbersToRomanNumerals(number) {
+  const romansToNumberPairs = {
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1,
+  };
 
-    return "I";
+for (const key of Object.keys(romansToNumberPairs)) {
+  if(romansToNumberPairs[key] === number) return key;
 }
-module.exports =convertNumbersToRomanNumerals;
+}
+
+module.exports = convertNumbersToRomanNumerals;
--- scr/numbersToRomanNumerals.test.js
+++ scr/numbersToRomanNumerals.test.js
@@ -1,5 +1,29 @@
 const convertNumbersToRomanNumerals = require('./numbersToRomanNumerals');
 
-test('return "I" when input is 1', () => {
-  expect(convertNumbersToRomanNumerals(1)).toBe("I");
+// test('return "I" when input is 1', () => {
+//   expect(convertNumbersToRomanNumerals(1)).toEqual("I");
+// });
+// test('return "V" when input is 5', () => {
+//   expect(convertNumbersToRomanNumerals(5)).toEqual("V");
+// });
+// test('return "X" when input is 10', () => {
+//   expect(convertNumbersToRomanNumerals(10)).toEqual("X");
+// });
+// test('return "C" when input is 50', () => {
+//   expect(convertNumbersToRomanNumerals(50)).toEqual("C");
+// });
+// test('return "C" when input is 50', () => {
+//   expect(convertNumbersToRomanNumerals(50)).toEqual("C");
+// });
+
+const cases = [[1,"I"], [5,"V"], [10,"X"], [50,"L"], [100,"C"]];
+
+describe("'add' utility", () => {
+  test.each(cases)(
+    "given %p arguments, returns %p",
+    (number, expectedResult) => {
+      const result = convertNumbersToRomanNumerals(number);
+      expect(result).toEqual(expectedResult);
+    }
+  );
 });
