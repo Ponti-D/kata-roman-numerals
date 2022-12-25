f5f22ceb03181ee4db00c141977b764b0fd11251
Ponti-D <spaediaz@gmail.com>
2022-12-25 11:49:03 +0000

    fixed code to handle repeated roman numerals letters such as 28 -> XXVIII

--- scr/numbersToRomanNumerals.js
+++ scr/numbersToRomanNumerals.js
@@ -1,8 +1,11 @@
 function convertNumbersToRomanNumerals(number) {
+  //create a place holder for conversion table
   const romansToNumberPairs = {
     C: 100,
+    XC: 90,
     L: 50,
     X: 10,
+    IX: 9,
     V: 5,
     IV: 4,
     I: 1,
@@ -10,18 +13,25 @@ function convertNumbersToRomanNumerals(number) {
 
   let romannumerals = "";
 
+  let numbersArray = Object.values(romansToNumberPairs);
+  let min = Math.min(...numbersArray);
+  //let romanstring = "";
   do {
     for (const key of Object.keys(romansToNumberPairs)) {
-      if (romansToNumberPairs[key] <= number) {
-        console.log(number);
-        number -= romansToNumberPairs[key]; // <-- 8/5 number = 3 <-- 3-1 = 2 <-- 2-1 =1 <--1-0
-        console.log(number);
+      //number = 8
+      let highestRomanNumrals = Math.max(
+        ...numbersArray.filter((e) => e <= number)
+      ); // max = 5
+      if (romansToNumberPairs[key] === highestRomanNumrals) {
+        console.log(highestRomanNumrals);
         romannumerals += key; //<-- "V" <--"VI" <--"VII" , --"VIII"
         console.log(romannumerals);
+        number -= highestRomanNumrals; // <-- 8-5 number = 3 <-- 3-1 = 2 <-- 2-1 =1 <--1-0
+        console.log(number);
       }
     }
-  } while (number > 0); //<--"V"
- 
+  } while (number > 0);
+  
   return romannumerals;
 }
 
--- scr/numbersToRomanNumerals.test.js
+++ scr/numbersToRomanNumerals.test.js
@@ -1,22 +1,7 @@
 const convertNumbersToRomanNumerals = require('./numbersToRomanNumerals');
 
-// test('return "I" when input is 1', () => {
-//   expect(convertNumbersToRomanNumerals(1)).toEqual("I");
-// });
-// test('return "V" when input is 5', () => {
-//   expect(convertNumbersToRomanNumerals(5)).toEqual("V");
-// });
-// test('return "X" when input is 10', () => {
-//   expect(convertNumbersToRomanNumerals(10)).toEqual("X");
-// });
-// test('return "C" when input is 50', () => {
-//   expect(convertNumbersToRomanNumerals(50)).toEqual("C");
-// });
-// test('return "C" when input is 50', () => {
-//   expect(convertNumbersToRomanNumerals(50)).toEqual("C");
-// });
-
-const cases = [[1,"I"], [5,"V"], [10,"X"], [50,"L"], [100,"C"],[4,"IV"],[8,"VIII"],[12,"XII"]];
+const cases = [[1,"I"], [5,"V"], [10,"X"], [50,"L"], [100,"C"],
+[8,"VIII"],[12,"XII"], [4,"IV"], [9,"IX"],[18,"XVIII"],[28,"XXVIII"]];
 
 describe("convert numbers to roman numerals", () => {
   test.each(cases)(
