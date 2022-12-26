6233deee1224a9326d716917342e4738f072d3e4
Ponti-D <spaediaz@gmail.com>
2022-12-26 13:56:04 +0000

    added 369 test case

--- scr/numbersToRomanNumerals.test.js
+++ scr/numbersToRomanNumerals.test.js
@@ -2,7 +2,7 @@ const convertNumbersToRomanNumerals = require('./numbersToRomanNumerals');
 
 const cases = [[1,"I"], [5,"V"], [10,"X"], [50,"L"], [100,"C"],
 [8,"VIII"],[12,"XII"], [4,"IV"], [9,"IX"],[18,"XVIII"],[28,"XXVIII"],
-[83,"LXXXIII"],[99,"XCIX"],[200,"CC"]];
+[83,"LXXXIII"],[99,"XCIX"],[200,"CC"],[369,"CCCLXIX"]];
 
 describe("convert numbers to roman numerals", () => {
   test.each(cases)(
